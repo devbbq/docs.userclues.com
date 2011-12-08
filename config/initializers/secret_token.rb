b1ff371f8331ea807cd6c7e9f1c9a64f9e9ec047
Kevin Sornberger <ksornberger@gmail.com>
2011-12-07 23:40:24 -0500

    Bare app for first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocsUsercluesCom::Application.config.secret_token = '0f51844c1a8c42db1dc5107e251885f6d87cae7b2c8a886dce86490acb5a8af0e8f4a73331981a58082c7b072e81361a07cecd19b876a43f7326de436207fe52'
